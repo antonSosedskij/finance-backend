d95d3854f96cbd5f01d49eabafd0fea363f5ed6c
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-09 19:24:38 +0300

    добавил простой контроллер для доходов

---
+++ finance-backend/Controllers/IncomeController.cs
@@ -0,0 +1,31 @@
+﻿using finance_backend.Data_access.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace finance_backend.Controllers;
+
+[ApiController]
+public class IncomeController
+{
+    public ApplicationContext db;
+    
+    public IncomeController(ApplicationContext context)
+    {
+        db = context;
+    }
+    
+    [HttpGet("api/incomes")]
+    public IEnumerable<Income> Get()
+    {
+        return db.incomes.ToList();
+    }
+    
+    [HttpPost("api/incomes")]
+    public Income Post(Income income)
+    {
+        
+        db.incomes.Add(income);
+        db.SaveChanges();
+        return income;
+    }
+}
