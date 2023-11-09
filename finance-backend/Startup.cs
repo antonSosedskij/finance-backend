5904dd82f2dcde79f9e30e2a42d9fb916231a49a
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-09 19:23:36 +0300

    настроена базовая конфигурация приложения

---
+++ finance-backend/Startup.cs
@@ -0,0 +1,63 @@
+﻿using finance_backend.Data_access.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+
+namespace finance_backend;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    public IConfiguration Configuration { get; }
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        string connection = Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
+        
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Finance API", Version = "v1" });
+        });
+
+        
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationContext dbContext)
+    {
+        
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+        
+        dbContext.Database.Migrate();
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Finance API V1");
+        });
+
+    }
+}
