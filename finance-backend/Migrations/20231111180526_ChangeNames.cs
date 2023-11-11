65c9c6879dcc2e6b86666a9632cd0cba71629dc8
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-11 22:44:49 +0300

    Поправил настройки

---
+++ finance-backend/Migrations/20231111180526_ChangeNames.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finance_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "incomes",
+                keyColumn: "id",
+                keyValue: new Guid("35e3fe02-85f3-4713-a745-3b598fad63f2"));
+
+            migrationBuilder.RenameColumn(
+                name: "categoryId",
+                table: "balances",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "balances",
+                newName: "categoryId");
+
+            migrationBuilder.InsertData(
+                table: "incomes",
+                columns: new[] { "id", "amount", "title" },
+                values: new object[] { new Guid("1355bfe6-6e4f-4cf7-97df-dfa1dac530e8"), 2000f, "Покупочки" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "incomes",
+                keyColumn: "id",
+                keyValue: new Guid("1355bfe6-6e4f-4cf7-97df-dfa1dac530e8"));
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "balances",
+                newName: "categoryId");
+
+            migrationBuilder.RenameColumn(
+                name: "categoryId",
+                table: "balances",
+                newName: "id");
+
+            migrationBuilder.InsertData(
+                table: "incomes",
+                columns: new[] { "id", "amount", "title" },
+                values: new object[] { new Guid("35e3fe02-85f3-4713-a745-3b598fad63f2"), 2000f, "Покупочки" });
+        }
+    }
+}
