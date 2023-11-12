020307c3e1d40d83e7e147a4daad5e4e3d984e1a
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-12 22:14:44 +0300

    Добавил конфигурацию для сущности пользователя

---
+++ finance-backend/Infrastructure/Data-access/EntitiesConfig/UserConfiguration.cs
@@ -0,0 +1,24 @@
+using finance_backend.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace finance_backend.DataAccess.Models;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id)
+            .HasColumnName("id");
+        builder.Property(e => e.Id)
+            .ValueGeneratedOnAdd();
+        builder.Property(e => e.Username)
+            .IsRequired()
+            .HasColumnName("username");
+        builder.Property(e => e.Email)
+            .IsRequired()
+            .HasColumnName("email");
+    }
+}
