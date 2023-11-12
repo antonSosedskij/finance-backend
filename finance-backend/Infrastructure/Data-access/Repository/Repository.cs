6e89fc6827b2c34d8a8b08300c20b6c6318814f0
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-12 22:08:57 +0300

    добавил реализацию сохранения записи в бд в Репозитории

---
+++ finance-backend/Infrastructure/Data-access/Repository/Repository.cs
@@ -0,0 +1,27 @@
+using finance_backend.Application.Repositories;
+using finance_backend.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace finance_backend.DataAccess.Models.Data_access.Repository;
+
+public class Repository<TEntity, TId> : IRepository<TEntity, TId>
+    where TEntity : BaseEntity<TId>
+{
+    protected readonly ApplicationContext _context;
+    
+    public Repository(ApplicationContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task Save(TEntity entity)
+    {
+        var entry = _context.Entry(entity);
+
+        if (entry.State == EntityState.Detached)
+        {
+            await _context.AddAsync(entity);
+        }
+        await _context.SaveChangesAsync();
+    }
+}
