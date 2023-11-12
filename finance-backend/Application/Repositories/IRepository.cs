806190c544456ca0cea71692826f67e4bc868915
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-12 22:08:05 +0300

    добавил интерфейс реализации Repository

---
+++ finance-backend/Application/Repositories/IRepository.cs
@@ -0,0 +1,8 @@
+using finance_backend.Domain.Shared;
+
+namespace finance_backend.Application.Repositories;
+
+public interface IRepository<TEntity, TId> where TEntity : BaseEntity<TId>
+{
+    Task Save(TEntity entity);
+}
