2d693b4df1bcceee2c9f43d20d566c4ad61ef29d
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-12 22:07:44 +0300

    добавил интерфейс реализации Identity

---
+++ finance-backend/Application/Identity/Interfaces/IIdentityService.cs
@@ -0,0 +1,9 @@
+using finance_backend.Application.Identity.Contracts;
+
+namespace finance_backend.Application.Identity.Interfaces;
+
+public interface IIdentityService
+{
+    Task<CreateUser.Response> CreateUser(CreateUser.Request request);
+    
+}
