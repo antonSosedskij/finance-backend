86269f96f243bb86abe1a6f8b270cd1376733d83
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-12 22:16:51 +0300

    добавил миграции

---
+++ finance-backend/Migrations/20231112134641_add_user.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finance_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "incomes",
+                keyColumn: "id",
+                keyValue: new Guid("fd0bb89b-bbfa-4704-b486-794ee0a25e62"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "incomes",
+                columns: new[] { "id", "amount", "CreatedDate", "RemovedDate", "title", "UpdatedDate" },
+                values: new object[] { new Guid("fd0bb89b-bbfa-4704-b486-794ee0a25e62"), 2000f, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Покупочки", null });
+        }
+    }
+}
---
+++ finance-backend/Migrations/20231112135611_add_user_v2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finance_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_user_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Lastname = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RemovedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
