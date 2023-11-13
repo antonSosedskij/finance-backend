32c33c6b53894c50ab7b89b13203037af761954e
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-13 10:36:43 +0300

    добавил эндпоинт на логин пользователя

--- finance-backend/API/Controllers/User/UserController.cs
+++ finance-backend/API/Controllers/User/UserController.cs
@@ -2,14 +2,13 @@ using finance_backend.API.Dto;
 using finance_backend.Application.Identity.Interfaces;
 using finance_backend.Application.Services.User.Contracts;
 using finance_backend.Application.Services.User.Interfaces;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace finance_backend.API.Controllers.User;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UserController : ControllerBase
+public partial class UserController : ControllerBase
 {
 
     private readonly IIdentityService _identityService;
