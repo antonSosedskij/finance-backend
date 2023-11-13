c96e788e24cdac1e4a45592be866478d4d08a572
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-13 22:28:36 +0300

    добавил миграции

---
+++ finance-backend/Migrations/20231113175821_SetCategoryBalanceRelation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finance_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetCategoryBalanceRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_balances_CategoryId",
+                table: "balances",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_balances_categories_CategoryId",
+                table: "balances",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_balances_categories_CategoryId",
+                table: "balances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_balances_CategoryId",
+                table: "balances");
+        }
+    }
+}
