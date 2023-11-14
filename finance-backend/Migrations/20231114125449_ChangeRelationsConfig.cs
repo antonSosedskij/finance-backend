fba1cdd7f42e97d28a808948e4775ca9d7d1e53c
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-14 16:52:28 +0300

    Добавил миграции

---
+++ finance-backend/Migrations/20231114125449_ChangeRelationsConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finance_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRelationsConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId1",
+                table: "categories",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_UserId1",
+                table: "categories",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_categories_users_UserId1",
+                table: "categories",
+                column: "UserId1",
+                principalTable: "users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_categories_users_UserId1",
+                table: "categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_categories_UserId1",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "categories");
+        }
+    }
+}
